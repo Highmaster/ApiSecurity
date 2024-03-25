f7bfccf12619b7648713761bd2a83fa5b70694bf
Highmaster <nzechideraann@gmail.com>
2024-03-25 23:54:26 +0100

    adding the entity package

---
+++ src/main/java/com/test/apisecurity/ApisecurityyApplication.java
@@ -0,0 +1,13 @@
+package com.test.apisecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisecurityyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisecurityyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/apisecurity/ApisecurityyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.apisecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisecurityyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
